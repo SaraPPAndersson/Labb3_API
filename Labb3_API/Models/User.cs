3712d4bacf6b8cd1753d8b6df6a654f19061c80e
Sara Andersson <paphatsara1995@hotmail.com>
2026-04-23 22:14:51 +0200

    Added .Where to fix bug

--- Labb3_API/Models/User.cs
+++ Labb3_API/Models/User.cs
@@ -1,16 +1,15 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace Labb3_API.Models
 {
     public class User
     {
         public int Id { get; set; }
+        [Required]
         public string? FullName { get; set; }
         public string? Email { get; set; }
         public string? Phone { get; set; }
-
-        //public Interest Interest { get; set; } = null!;
-        //[JsonIgnore]
         public ICollection<Link> Links { get; set; } = null!;
     }
 }
