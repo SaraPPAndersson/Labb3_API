0a7bc8bb3a7c14998ff29dc1081697a82bad1290
Sara Andersson <paphatsara1995@hotmail.com>
2026-04-27 09:59:44 +0200

    fixed bug

---
+++ Labb3_API/Migrations/20260424140859_changedInterestName.cs
@@ -0,0 +1,155 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Labb3_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class changedInterestName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Interests");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Interests",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Interests",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.UpdateData(
+                table: "Interests",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Title",
+                value: "Programmering");
+
+            migrationBuilder.UpdateData(
+                table: "Interests",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Title",
+                value: "Träning");
+
+            migrationBuilder.UpdateData(
+                table: "Interests",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Title",
+                value: "Musik");
+
+            migrationBuilder.UpdateData(
+                table: "Interests",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Title",
+                value: "Resor");
+
+            migrationBuilder.UpdateData(
+                table: "Interests",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Title",
+                value: "Matlagning");
+
+            migrationBuilder.UpdateData(
+                table: "Interests",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "Title",
+                value: "Gaming");
+
+            migrationBuilder.UpdateData(
+                table: "Interests",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "Title",
+                value: "Läsning");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Interests");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Interests",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Interests",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Interests",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Name",
+                value: "Programmering");
+
+            migrationBuilder.UpdateData(
+                table: "Interests",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Name",
+                value: "Träning");
+
+            migrationBuilder.UpdateData(
+                table: "Interests",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Name",
+                value: "Musik");
+
+            migrationBuilder.UpdateData(
+                table: "Interests",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Name",
+                value: "Resor");
+
+            migrationBuilder.UpdateData(
+                table: "Interests",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Name",
+                value: "Matlagning");
+
+            migrationBuilder.UpdateData(
+                table: "Interests",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "Name",
+                value: "Gaming");
+
+            migrationBuilder.UpdateData(
+                table: "Interests",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "Name",
+                value: "Läsning");
+        }
+    }
+}
